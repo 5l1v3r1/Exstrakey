bfd0c8d7f866fa4357cdf58e1588a66ea3f2d3fa
The Sixty Nine <52529306+thesixtynine@users.noreply.github.com>
2019-10-20 11:31:47 +0700

    Update key.py

--- key.py
+++ key.py
@@ -1,1 +1,63 @@
+import os, sys, time
+from time import sleep
 
+def banner():
+    os.system('clear')
+    os.system('reset')
+    time.sleep(1)
+
+banner()
+
+def write(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+logo = """\x1b[96;2;3m
+
+  ████████╗███████╗██████╗ ███╗   ███╗██╗   ██╗██╗  ██╗
+  ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║   ██║╚██╗██╔╝
+     ██║   █████╗  ██████╔╝██╔████╔██║██║   ██║ ╚███╔╝
+     ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║   ██║ ██╔██╗
+     ██║   ███████╗██║  ██║██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗
+     ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝
+"""
+print (logo)
+print ('')
+
+print
+print ('\x1b[0m[\x1b[92;1m#\x1b[0m] \x1b[77;1mShorcut for help you')
+print ('\x1b[0m[\x1b[95;1m*\x1b[0m] \x1b[77;1mModified By Nedi Senja')
+print ('')
+
+write ('\x1b[0m[\x1b[91;1m!\x1b[0m] \x1b[77;1mChecking ...')
+print ('\x1b[0m[\x1b[92;1m+\x1b[0m] \x1b[0mProcess Reloads')
+print ('')
+sleep (3)
+write ('\x1b[0m[\x1b[93;1m!\x1b[0m] \x1b[77;1mMaking termux properties directory ...')
+sleep (3)
+try:
+      os.mkdir('/data/data/com.termux/files/home/.termux')
+except:
+      pass
+print ('\x1b[0m[\x1b[94;1m+\x1b[0m] \x1b[0mSuccess')
+print ('')
+sleep (3)
+write ('\x1b[0m[\x1b[95;1m+\x1b[0m] \x1b[77;1mMaking setup file ...')
+sleep (1)
+
+key = "extra-keys = [['ESC','/','-','HOME','UP','END','|','clear','exit'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','su','_','tor']]"
+control = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
+control.write(key)
+control.close()
+sleep (1)
+print ('\x1b[0m[\x1b[96;1m!\x1b[0m] \x1b[0mSuccess')
+print ('')
+sleep (3)
+write ('\x1b[0m[\x1b[97;1m!\x1b[0m] \x1b[77;1mSetting up ...')
+sleep (1)
+print ('\x1b[0m[\x1b[90;1m+\x1b[0m] \x1b[0mSuccesfully')
+os.system('termux-reload-settings')
+print ('')
+print ('')
